23c72409127fd293032ee7ed21b2be2cdc720eb1
Sungkuk Kim <sungkuk5420@gmail.com>
2023-09-02 12:05:49 +0900

    라라벨 프로젝트 진행중

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NewController
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// echo "블레이드 페이지 파일을 불러오기 바로전 <br/>";
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('get',[NewController::class,"index"]);
